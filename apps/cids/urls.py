009a6f21abb05350843aec4206e8abd61968ff2a
malimaaa-hue <milenaalvesdelimaaa@gmail.com>
2026-05-29 20:59:43 -0300

    Otimização dos templates core e cids, além de algumas correções de código.

--- apps/cids/urls.py
+++ apps/cids/urls.py
@@ -6,6 +6,6 @@ app_name = 'cids'
 urlpatterns = [
     path('adicionar/', views.add_cid, name='add_cid'),
     path('listar/', views.list_cids, name='list_cids'),
-    path('editar/<int:id_cid/', views.edit_cid, name='edit_cid'),
+    path('editar/<int:id_cid>/', views.edit_cid, name='edit_cid'),
     path('excluir/<int:id_cid>/', views.delete_cid, name='delete_cid'),
 ]

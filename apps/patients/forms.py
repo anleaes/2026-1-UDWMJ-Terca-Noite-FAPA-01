7bb873cd1bf245ecdd9a5bd919f378c6e377dce6
Lucas Amui <lukeamui@hotmail.com>
2026-06-07 17:35:35 -0300

    Implementa novas features ao Histórico Médico e criação de novos Pacientes

--- apps/patients/forms.py
+++ apps/patients/forms.py
@@ -1,7 +1,15 @@
 from django import forms
 from .models import Patient
- 
+
 class PatientForm(forms.ModelForm):
     class Meta:
         model = Patient
         exclude = ()
+
+class PatientCreateForm(forms.ModelForm):
+    allergies_text    = forms.CharField(label='Alergias', widget=forms.Textarea, required=False)
+    family_history_text = forms.CharField(label='Histórico Familiar', widget=forms.Textarea, required=False)
+
+    class Meta:
+        model = Patient
+        exclude = ('allergies',)

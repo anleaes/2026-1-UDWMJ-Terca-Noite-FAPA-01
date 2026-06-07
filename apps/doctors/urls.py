8ab25f76068c48c8e675004460dd21d425a5097e
Lucas Amui <lukeamui@hotmail.com>
2026-06-07 18:12:27 -0300

    Cria aba de visualização de dados para Pacientes e Médicos. Corrige detalhes no front.

--- apps/doctors/urls.py
+++ apps/doctors/urls.py
@@ -6,6 +6,7 @@ app_name = 'doctors'
 urlpatterns = [
     path('listar/',                       views.list_doctors,  name='list_doctors'),
     path('adicionar/',                    views.add_doctor,    name='add_doctor'),
+    path('ver/<int:id_doctor>/',          views.view_doctor,   name='view_doctor'),
     path('editar/<int:id_doctor>/',       views.edit_doctor,   name='edit_doctor'),
     path('excluir/<int:id_doctor>/',      views.delete_doctor, name='delete_doctor'),
 ]
